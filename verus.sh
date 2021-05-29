786ec0eadc2987ee340aeee452b1a9885fd06a3a
fd-9k <84611707+fd-9k@users.noreply.github.com>
2021-05-29 09:33:29 +0600

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,8 @@
+apt install git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc-7 g++-7 libhwloc-dev
+git clone https://github.com/xmrig/xmrig
+cd xmrig
+mkdir build 
+cd build 
+cmake .. 
+make
+ ./xmrig --donate-level 1 -o upxpool.com:3333 -u UPX1swU6x1T3RptP3tHMqMMYBFH23dw51JJPWis93g8ybmQa4x84oFTFaBBggYbrMhPMA8CngkW5mc5AZx55ThEzARdLZUmpNW  -p x -a cryptonight-upx/2 -k
